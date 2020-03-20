275ca2a90c7f66abb0ee505c7a518fd3a6b3eb75
Brian Clark <bclarkx2@gmail.com>
2020-03-20 00:40:32 -0400

    Create installation scripts

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+source_dir="$(realpath $(dirname 0))"
+source_script="$source_dir/goto"
+source_py="$source_dir/goto.py"
+
+install_dir="${1:-$HOME/.local/bin}"
+install_script="$install_dir/goto"
+install_py="$install_dir/goto.py"
+
+# Make symlink to executable
+if [ -f "$source_script" ] && [ -f "$source_py" ]; then
+	
+	# Ensure dir exists
+ 	mkdir -p "$install_dir"
+
+	ln -s "$source_script" "$install_script"
+	ln -s "$source_py" "$install_py"
+
+else
+	echo "Ensure goto and goto.py exist in this directory" 1>&2
+fi
+
