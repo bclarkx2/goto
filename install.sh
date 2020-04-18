9ac9900cfc003440a66c638f81b0dd6b62208c69
Brian Clark <bclarkx2@gmail.com>
2020-04-18 13:36:51 -0400

    Remove interaction from setup step and add it to install process

--- install.sh
+++ install.sh
@@ -14,8 +14,12 @@ if [ -f "$source_script" ] && [ -f "$source_py" ]; then
 	# Ensure dir exists
  	mkdir -p "$install_dir"
 
+  # Create symlinks
 	ln -s "$source_script" "$install_script"
 	ln -s "$source_py" "$install_py"
+
+  # Call setup function
+  "$install_script" --setup
 
 else
 	echo "Ensure goto and goto.py exist in this directory" 1>&2
